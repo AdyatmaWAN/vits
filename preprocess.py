e282377670fbedc03a4b0c14163dcf6c38a3d40a
Adyatma <adyatma.wijaksara@gmail.com>
2024-10-24 18:07:51 +0700

    save each line and not in bulk

--- preprocess.py
+++ preprocess.py
@@ -10,16 +10,17 @@ if __name__ == '__main__':
   parser.add_argument("--text_cleaners", nargs="+", default=["english_cleaners2"])
 
   args = parser.parse_args()
-    
 
   for filelist in args.filelists:
     print("START:", filelist)
     filepaths_and_text = load_filepaths_and_text(filelist)
-    for i in range(len(filepaths_and_text)):
-      original_text = filepaths_and_text[i][args.text_index]
-      cleaned_text = text._clean_text(original_text, args.text_cleaners)
-      filepaths_and_text[i][args.text_index] = cleaned_text
 
     new_filelist = filelist + "." + args.out_extension
     with open(new_filelist, "w", encoding="utf-8") as f:
-      f.writelines(["|".join(x) + "\n" for x in filepaths_and_text])
+      for i in range(len(filepaths_and_text)):
+        original_text = filepaths_and_text[i][args.text_index]
+        cleaned_text = text._clean_text(original_text, args.text_cleaners)
+        filepaths_and_text[i][args.text_index] = cleaned_text
+
+        # Save each line individually
+        f.write("|".join(filepaths_and_text[i]) + "\n")
